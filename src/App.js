3529b7b65293e06c7c49e1ce9a35dacf454582e7
dilantha111 <dilantha.prasanjith111@gmail.com>
2020-04-19 13:16:32 +0530

    step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> {count} </h1>
+      <button onClick={() => setCount(count + 1)}> Click </button>
     </div>
   );
 }
