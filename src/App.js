98608a3d47a473563e236aedf62acc9b47c22e92
dilantha111 <dilantha.prasanjith111@gmail.com>
2020-04-19 14:08:54 +0530

    step 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import * as axios from "axios";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [advice, setAdvice] = useState(null);
+
+  const fetchAnAdvice = async () => {
+    try {
+      const { data } = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
-      <h1> {count} </h1>
-      <button onClick={() => setCount(count + 1)}> Click </button>
+      <h1> {advice} </h1>
+      <button onClick={fetchAnAdvice}> Get a new Advice </button>
     </div>
   );
 }
