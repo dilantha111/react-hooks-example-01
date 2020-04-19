de081ce8af20dbc07cc4df26e525c4baaf269227
dilantha111 <dilantha.prasanjith111@gmail.com>
2020-04-19 15:59:43 +0530

    step 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as axios from "axios";
 import "./styles.css";
 
 export default function App() {
   const [advice, setAdvice] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchAnAdvice();
+  }, []);
 
   const fetchAnAdvice = async () => {
     try {
+      setIsLoading(true);
       const { data } = await axios.get("https://api.adviceslip.com/advice");
       setAdvice(data.slip.advice);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -16,7 +23,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1> {advice} </h1>
+      <h1> {isLoading ? '...': advice} </h1>
       <button onClick={fetchAnAdvice}> Get a new Advice </button>
     </div>
   );
